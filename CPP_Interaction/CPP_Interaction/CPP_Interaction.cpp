d736d3fb34f27b5306818ce9d75988961df189da
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-04-07 08:47:44 +0200

    feat(CPP_Interaction): Add functions

--- CPP_Interaction/CPP_Interaction/CPP_Interaction.cpp
+++ CPP_Interaction/CPP_Interaction/CPP_Interaction.cpp
@@ -1,31 +1,30 @@
 #include <Windows.h>
 #include <iostream>
 #include <conio.h>
+#include <functional>
 
-static void LoopThings();
+static void LoopThings(std::function<void()> callback);
 static void LogThings();
 static std::pair<int, int> GetCursorPosition();
+static void SimulateLeftClick(int numberOfClicks);
+static void SimulateLeftClickAtSpecifiedCursorPositionOnCommand();
 
 int main()
 {
-	std::cout << "This program logs the position of the cursor every time you input a key. "
-		<< "Press 'Enter' at any time to exit." << std::endl;
-
-	LoopThings();
+	//LoopThings([]() { LogThings(); });
+	//LoopThings([]() { SimulateLeftClickAtSpecifiedCursorPositionOnCommand(); });
+	LoopThings([]() { SimulateLeftClick(10); });
 	
 	return 0;
 }
 
-static void LoopThings()
+static void LoopThings(std::function<void()> callback)
 {
-	char input;
+	std::cout << "Press 'Enter' at any time to exit." << std::endl;
 
-	while (input = _getch())
+	while (_getch() != '\r')
 	{
-		if (input == '\r')
-			break;
-
-		LogThings();
+		callback();
 	}
 }
 
@@ -47,4 +46,28 @@ static std::pair<int, int> GetCursorPosition()
 		currentCursorPosValues.second = cursorPosition.y;
 	}
 	return currentCursorPosValues;
+}
+
+static void SimulateLeftClick(int numberOfClicks)
+{
+	for (int i = 0; i < numberOfClicks; i++)
+	{
+		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+	}
+}
+
+static void SimulateLeftClickAtSpecifiedCursorPositionOnCommand()
+{
+	std::cout << "First, press a key when you're hovering your cursor above a position. "
+		<< "Then, press the key again after moving your cursor away from the position." << std::endl;
+
+	char input = _getch();
+
+	std::pair<int, int> savedCursorPosition = GetCursorPosition();
+
+	while (_getch() != input) { }
+
+	SetCursorPos(savedCursorPosition.first, savedCursorPosition.second);
+	SimulateLeftClick(1);
 }
