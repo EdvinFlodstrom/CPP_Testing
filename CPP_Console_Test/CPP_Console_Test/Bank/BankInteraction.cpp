f8864f722068ee87d8a533a3773ab15e9933b2e6
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-05-23 10:02:20 +0200

    chore(CPP_Console_Test): Add project files

---
+++ CPP_Console_Test/CPP_Console_Test/Bank/BankInteraction.cpp
@@ -0,0 +1,9 @@
+#include "BankInteraction.h"
+#include "BankAccount.h"
+
+int bankInteraction()
+{
+
+
+	return 0;
+}
--- CPP_Console_Test/CPP_Console_Test/CPP_Console_Test.cpp
+++ CPP_Console_Test/CPP_Console_Test/CPP_Console_Test.cpp
@@ -1,6 +1,9 @@
+#include "Bank/BankInteraction.h"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+    return bankInteraction();
 }
