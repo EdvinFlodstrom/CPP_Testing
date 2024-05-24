d0538ddaf2b865d09241e240f514176cb9cf1025
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-05-24 16:00:36 +0200

    feat(CPP_Console_Test): Add bank logic

--- CPP_Console_Test/CPP_Console_Test/Bank/BankAccount.cpp
+++ CPP_Console_Test/CPP_Console_Test/Bank/BankAccount.cpp
@@ -4,8 +4,8 @@
 
 auto inputAmountIsValid = [](double inputAmount) -> bool { return inputAmount > 0; };
 
-const std::string BAD_DEPOSIT_AMOUNT_STRING = "The requested amount may not be 0 or below.";
-const std::string BAD_WITHDRAW_AMOUNT_STRING = "You may not withdraw more money than you have in your bank.";
+const std::string BAD_DEPOSIT_AMOUNT_STRING = "The requested amount may not be 0 or below.\n";
+const std::string BAD_WITHDRAW_AMOUNT_STRING = "You may not withdraw more money than you have in your bank.\n";
 
 BankAccount::BankAccount(int accountNumber, std::string accountHolderName, double balance) 
 	: accountNumber(accountNumber), accountHolderName(accountHolderName), balance(balance) { }
--- CPP_Console_Test/CPP_Console_Test/Bank/BankInteraction.cpp
+++ CPP_Console_Test/CPP_Console_Test/Bank/BankInteraction.cpp
@@ -8,6 +8,16 @@
 int CURRENT_ACCOUNT_NUMBER = 1;
 int const INITIAL_BALANCE = 500;
 
+int convertStringToInt(std::string numberAsString) {
+	try {
+		return std::stoi(numberAsString);
+	}
+	catch (const std::invalid_argument& e) {
+		std::cerr << "Invalid input. Please enter a valid integer.\n";
+		return 0;
+	}
+}
+
 static BankAccount* findBankAccount(std::vector<BankAccount*>& bankAccounts, int accountNumber) {
 	for (auto account : bankAccounts) {
 		if (account->getAccountNumber() == accountNumber)
@@ -23,11 +33,10 @@ static BankAccount* getBankAccountConsoleInput(std::vector<BankAccount*>& bankAc
 	int accountNumber = 0;
 	std::string accountNumberString;
 
-	if (std::cin >> accountNumberString) {
-		accountNumber = std::stoi(accountNumberString);
-	}
-	else {
-		std::cerr << "Invalid input. Please enter an integer.\n";
+	std::cin >> accountNumberString;
+	accountNumber = convertStringToInt(accountNumberString);
+	
+	if (accountNumber <= 0) {
 		return nullptr;
 	}
 
@@ -37,6 +46,20 @@ static BankAccount* getBankAccountConsoleInput(std::vector<BankAccount*>& bankAc
 		std::cerr << "Bank account not found. Please try again.\n";
 	}
 	return bankAccount;
+}
+
+void depositOrWithdrawMoney(BankAccount& bankAccount, bool deposit) {
+	double amount;
+	std::string stringAmount;
+
+	std::cout << "Please input the amount of money you would like to " << (deposit ? "deposit" : "withdraw") << ": $";
+
+	std::cin >> stringAmount;
+	amount = convertStringToInt(stringAmount);
+
+	if (amount > 0) {
+		std::cout << (deposit ? bankAccount.deposit(amount) : bankAccount.withdraw(amount)) << "\n";
+	}
 }
 
 int bankInteraction() {
@@ -84,9 +107,20 @@ int bankInteraction() {
 		else if (choice == 3) {
 			std::cout << "Depositing money...\n";
 
+			BankAccount* bankAccount = getBankAccountConsoleInput(bankAccounts);
+
+			if (bankAccount != nullptr) {
+				depositOrWithdrawMoney(*bankAccount, true);
+			}
 		}
 		else if (choice == 4) {
 			std::cout << "Withdrawing money...\n";
+
+			BankAccount* bankAccount = getBankAccountConsoleInput(bankAccounts);
+
+			if (bankAccount != nullptr) {
+				depositOrWithdrawMoney(*bankAccount, false);
+			}
 
 		}
 		else {
